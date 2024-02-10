797d40c38be9c922cd4a28ebfc4d4bdabd7ad91a
gogeterobert <gogeterobert@yahoo.com>
2024-02-10 19:06:38 +0100

    Added service for adding, deleting and querying a student and updated endpoint to use the new functionality
    Signed-off-by: gogeterobert <gogeterobert@yahoo.com>

--- swagger_server/controllers/default_controller.py
+++ swagger_server/controllers/default_controller.py
@@ -1,3 +1,5 @@
+from swagger_server.service.student_service import *
+
 import connexion
 import six
 
@@ -16,8 +18,9 @@ def add_student(body=None):  # noqa: E501
     :rtype: float
     """
     if connexion.request.is_json:
-        body = Student.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
+        body = Student.from_dict(connexion.request.get_json()) # noqa: E501
+        return add(body)
+    return 500,'error'
 
 
 def delete_student(student_id):  # noqa: E501
@@ -30,7 +33,7 @@ def delete_student(student_id):  # noqa: E501
 
     :rtype: None
     """
-    return 'do some magic!'
+    return delete(student_id)
 
 
 def get_student_by_id(student_id):  # noqa: E501
@@ -43,4 +46,4 @@ def get_student_by_id(student_id):  # noqa: E501
 
     :rtype: Student
     """
-    return 'do some magic!'
+    return get_by_id(student_id)
